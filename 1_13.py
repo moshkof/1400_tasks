c1e7d87c55e9f395267a2063113ce02b60cd3669
cataclysm78 <42520898+moshkof@users.noreply.github.com>
2020-12-18 14:24:00 +0300

    решина задача 1_13

---
+++ 1_13.py
@@ -0,0 +1,3 @@
+number = int(input('введите число: '))
+print('следующее за числом ' + str(number) + ',' + ' число: ' + str(number + 1))
+print('предыдущее число для числа ' + str(number) + ',' + ' число: ' + str(number - 1))
